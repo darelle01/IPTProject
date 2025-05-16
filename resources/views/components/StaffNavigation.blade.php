1dfbb905e679c4ce24b06a8ed162222455b117ba
you <you@example.com>
2025-05-16 22:12:00 +0800

    05/16/2025 new

--- resources/views/components/StaffNavigation.blade.php
+++ resources/views/components/StaffNavigation.blade.php
@@ -8,7 +8,7 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @vite(['resources/sass/app.scss','resources/js/app.js'])
     {{-- Bootstrap --}}
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     {{-- Fontawesome --}}
     {{-- <script src="https://kit.fontawesome.com/6d462838cf.js" crossorigin="anonymous"></script> --}}
     {{-- Css --}}
